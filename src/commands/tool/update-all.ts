2e4ec40455afed4fcd060437b862d4328f4e0f51
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-10-23 02:44:35 +0200

    feat: implement tool update and update-all commands

--- src/commands/tool/update-all.ts
+++ src/commands/tool/update-all.ts
@@ -1,6 +1,10 @@
-import { Command } from "@oclif/core";
+import { CliUx } from "@oclif/core";
+import { R } from "../../api/utils/result";
+import BaseCommand from "../../utils/base-command";
 
-export default class ToolUpdateAll extends Command {
+export default class ToolUpdateAllCommand extends BaseCommand<
+  typeof ToolUpdateAllCommand
+> {
   static summary = "Update all tools to their last version supported by Bazar.";
   static description =
     "The new versions of the tools will be downloaded by SMWCentral.";
@@ -8,6 +12,14 @@ export default class ToolUpdateAll extends Command {
   static examples = [];
 
   async run(): Promise<void> {
-    this.log("Not implemented");
+    CliUx.ux.action.start("Updating tools");
+    const response = await this.api.tool.updateAll();
+    if (R.isOk(response)) {
+      CliUx.ux.action.stop();
+      return;
+    }
+
+    const messages = R.messages(response, { verbose: true });
+    this.Error(`Failed to update tools\n${messages}`, 1);
   }
 }
--- src/commands/tool/update.ts
+++ src/commands/tool/update.ts
@@ -1,6 +1,12 @@
-import { Command } from "@oclif/core";
+import { CliUx } from "@oclif/core";
+import ToolManager, { ToolManagerError } from "../../api/managers/tool-manager";
+import { SupportedToolName } from "../../api/managers/tool-manager/supported-tool";
+import { R } from "../../api/utils/result";
+import BaseCommand from "../../utils/base-command";
 
-export default class ToolUpdate extends Command {
+export default class ToolUpdateCommand extends BaseCommand<
+  typeof ToolUpdateCommand
+> {
   static summary =
     "Update a given tool to the last version supported by Bazar.";
   static description =
@@ -8,7 +14,40 @@ export default class ToolUpdate extends Command {
 
   static examples = [];
 
+  static args = [
+    {
+      name: "tool-name",
+      required: true,
+      description: "Name of the tool",
+      options: ToolManager.ToolNames,
+    },
+  ];
+
   async run(): Promise<void> {
-    this.log("Not implemented");
+    const { args } = await this.parse(ToolUpdateCommand);
+
+    const toolName: SupportedToolName = args["tool-name"];
+
+    CliUx.ux.action.start(`Updating ${toolName}`);
+    const response = await this.api.tool.update(toolName);
+    if (R.isOk(response)) {
+      CliUx.ux.action.stop();
+      return;
+    }
+
+    if (response.code === ToolManagerError.ToolIsUpToDate) {
+      this.Warn(`${toolName} is up to date!`);
+      CliUx.ux.action.stop("interrupted");
+      return;
+    }
+
+    if (response.code === ToolManagerError.ToolNotInstalled) {
+      this.Warn(`${toolName} is not installed!`);
+      CliUx.ux.action.stop("interrupted");
+      return;
+    }
+
+    const messages = R.messages(response, { verbose: true });
+    this.Error(`Failed to update ${toolName}\n${messages}`, 1);
   }
 }
