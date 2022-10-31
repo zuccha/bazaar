e4946029810be80a49d18c0a6e6abe8346cb3342
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-10-31 14:32:14 +0100

    feat(api): add directory utility methods on Manager class

--- src/api/utils/manager.ts
+++ src/api/utils/manager.ts
@@ -1,4 +1,5 @@
 import { FS } from "./fs";
+import { ResultVoid } from "./result";
 
 export type ManagerBag = {
   fs: FS;
@@ -18,11 +19,27 @@ export default abstract class Manager {
     this.log = log;
   }
 
+  protected get name(): string {
+    return this.fs.getName(this.directoryPath);
+  }
+
   protected path(...paths: string[]): string {
     return this.fs.join(this.directoryPath, ...paths);
   }
 
   protected scope(functionName: string): string {
     return `${this.id}.${functionName}`;
+  }
+
+  protected async exists(): Promise<boolean> {
+    return this.fs.exists(this.directoryPath);
+  }
+
+  protected async createDirectory(): Promise<ResultVoid> {
+    return this.fs.createDirectory(this.directoryPath);
+  }
+
+  protected async removeDirectory(): Promise<ResultVoid> {
+    return this.fs.removeDirectory(this.directoryPath);
   }
 }
