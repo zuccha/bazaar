ae6793523fb01cd749e0fe761cc3327a7aa0d462
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-10-31 14:33:12 +0100

    feat(api): create project api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,13 @@
 import EditorManager from "./managers/editor-manager";
 import OriginalRomManager from "./managers/original-rom-manager";
+import ProjectManager from "./managers/project-manager";
 import ToolManager from "./managers/tool-manager";
 import { FS } from "./utils/fs";
 
 export default class Api {
+  readonly fs: FS;
+  readonly log: (message: string) => void;
+
   readonly editor: EditorManager;
   readonly originalRom: OriginalRomManager;
   readonly tool: ToolManager;
@@ -19,6 +23,9 @@ export default class Api {
   }) {
     const managerBag = { fs, log };
 
+    this.fs = fs;
+    this.log = log;
+
     this.editor = new EditorManager(
       fs.join(cacheDirectoryPath, "Editors"),
       managerBag,
@@ -33,5 +40,10 @@ export default class Api {
       fs.join(cacheDirectoryPath, "Tools"),
       managerBag,
     );
+  }
+
+  project(name: string, path: string): ProjectManager {
+    const managerBag = { fs: this.fs, log: this.log };
+    return new ProjectManager(this.fs.join(path, name), managerBag);
   }
 }
