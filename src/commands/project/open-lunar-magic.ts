a4c1428e94c2cba5a0c6c79f2da3905bcd195c72
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-11-01 23:10:31 +0100

    feat(cli): implement open lunar magic command

--- src/commands/project/open-lunar-magic.ts
+++ src/commands/project/open-lunar-magic.ts
@@ -1,15 +1,48 @@
-import { Command } from "@oclif/core";
+import LunarMagic from "../../api/managers/tool-collection/tools/lunar-magic";
+import { R } from "../../api/utils/result";
+import { ProjectFlags } from "../../commands-utils/project";
+import BaseCommand from "../../utils/base-command";
 
-export default class ProjectOpenLunarMagic extends Command {
-  static summary = "Open the ROM in Lunar Magic.";
+export default class ProjectOpenLunarMagicCommand extends BaseCommand<
+  typeof ProjectOpenLunarMagicCommand
+> {
+  static summary = "Open the ROM in Lunar Magic";
   static description = `\
-Open the ROM in Lunar Magic.
-If Lunar Magic is not installed, this command will fail. To install Lunar\
- Magic, run \`bazaar tool install lunar-magic\`.`;
+If Lunar Magic is not installed, this command will fail.
 
-  static examples = [];
+To install Lunar Magic, run \`bazaar tool install lunar-magic\`.`;
+
+  static examples = [
+    "bazaar project open-emulator",
+    "bazaar project open-emulator --path=C:\\Users\\me\\Documents\\MyProject",
+  ];
+
+  static flags = {
+    ...ProjectFlags,
+  };
 
   async run(): Promise<void> {
-    this.log("Not implemented");
+    const { flags } = await this.parse(ProjectOpenLunarMagicCommand);
+
+    this.Info.start(`Opening ROM hack in Lunar Magic`);
+    const project = this.api.project(flags.path);
+    const result = await project.openLunarMagic();
+
+    if (R.isOk(result)) {
+      this.Info.success();
+      return;
+    }
+
+    if (result.code === LunarMagic.ErrorCode.ToolIsNotUpToDate) {
+      this.Info.failure();
+      const message = `Lunar Magic is not installed or up-to-date
+Check \`bazaar tool install lunar-magic --help\` for more`;
+      this.Warning.log(message);
+      return;
+    }
+
+    this.Info.failure();
+    const messages = R.messages(result, { verbose: true });
+    this.Error(`Failed to open ROM hack in Lunar Magic\n${messages}`, 1);
   }
 }
