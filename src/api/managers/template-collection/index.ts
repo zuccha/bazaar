755defb32a7e5488dc31be17ca0e80aa252189ad
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-11-03 05:41:14 +0100

    feat(api): implement list project templates

--- src/api/managers/template-collection/index.ts
+++ src/api/managers/template-collection/index.ts
@@ -1,8 +1,19 @@
+import { R, Result } from "../../utils/result";
 import Directory from "../directory";
 import { ResourceBag } from "../resource";
 import ProjectTemplate from "./templates/project-template";
 
+export type TemplateInfo = {
+  name: string;
+};
+
+const ErrorCode = {
+  Generic: "TemplateCollection.Generic",
+};
+
 export default class TemplateCollection extends Directory {
+  static ErrorCode = ErrorCode;
+
   protected id = "TemplateCollection";
 
   private _bag: ResourceBag;
@@ -12,7 +23,33 @@ export default class TemplateCollection extends Directory {
     this._bag = bag;
   }
 
+  projectsDirectoryPath(...paths: string[]): string {
+    return this.path("Projects", ...paths);
+  }
+
+  async listProjects(): Promise<Result<TemplateInfo[]>> {
+    const scope = this.scope("listProjects");
+
+    this.logger.start(
+      `Getting "${this.projectsDirectoryPath()}" directory info`,
+    );
+    const directoryInfoResult = await this.fs.getDirectoryInfo(
+      this.projectsDirectoryPath(),
+    );
+    if (R.isError(directoryInfoResult)) {
+      this.logger.failure();
+      const message = `Failed to get "${this.projectsDirectoryPath()}" directory info`;
+      return R.Stack(directoryInfoResult, scope, message, ErrorCode.Generic);
+    }
+    this.logger.success();
+
+    const projectTemplateInfos = directoryInfoResult.data.directoryNames.map(
+      (directoryName) => ({ name: directoryName }),
+    );
+    return R.Ok(projectTemplateInfos);
+  }
+
   project(name: string): ProjectTemplate {
-    return new ProjectTemplate(this.path("Projects", name), this._bag);
+    return new ProjectTemplate(this.projectsDirectoryPath(name), this._bag);
   }
 }
