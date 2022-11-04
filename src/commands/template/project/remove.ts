218b1d70e89af48c48cbfb723ba3f91810f0254b
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-11-04 02:50:38 +0100

    feat(cli): implement template project remove command

--- src/commands/template/project/remove.ts
+++ src/commands/template/project/remove.ts
@@ -1,13 +1,48 @@
-import { Command } from "@oclif/core";
+import Project from "../../../api/managers/project";
+import { R } from "../../../api/utils/result";
+import { ProjectTemplateFlags } from "../../../commands-utils/project";
+import BaseCommand from "../../../utils/base-command";
 
-export default class TemplateProjectRemove extends Command {
-  static summary = "Remove a project from the list of available templates.";
-  static description =
-    "Removing a project from the list of available templates will not impact any project that used it as a starting point.";
+export default class TemplateProjectRemoveCommand extends BaseCommand<
+  typeof TemplateProjectRemoveCommand
+> {
+  static summary = "Remove a project from the list of available templates";
+  static description = `\
+N.B.: This action is not reversible!
 
-  static examples = [];
+Removing a project from the list of available templates will not impact any project that used it as a starting point.`;
+
+  static examples = ["bazaar template project remove --name=RHR"];
+
+  static flags = ProjectTemplateFlags;
 
   async run(): Promise<void> {
-    this.log("Not implemented");
+    const { flags } = await this.parse(TemplateProjectRemoveCommand);
+
+    this.Info.start("Removing project template");
+    const projectTemplate = this.api.templates.project(flags.name);
+    const result = await projectTemplate.remove();
+
+    if (R.isOk(result)) {
+      this.Info.success();
+      return;
+    }
+
+    if (result.code === Project.ErrorCode.ProjectNotFound) {
+      this.Info.failure();
+      this.Warning.log(`The project template "${flags.name}" does not exist`);
+      return;
+    }
+
+    if (result.code === Project.ErrorCode.ProjectNotValid) {
+      this.Info.failure();
+      const message = `The project template "${flags.name}" is not valid (missing baserom, invalid config, etc.)`;
+      this.Warning.log(message);
+      return;
+    }
+
+    this.Info.failure();
+    const messages = R.messages(result, { verbose: true });
+    this.Error(`Failed to remove project template\n${messages}`, 1);
   }
 }
