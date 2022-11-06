e488596b69916110a0d1429039f16da4c00a150e
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-11-06 17:55:53 +0100

    fix(api): fix patch collection types

--- src/api/managers/project/patch-collection/index.ts
+++ src/api/managers/project/patch-collection/index.ts
@@ -1,12 +1,8 @@
 import { ResourceBag } from "../../resource";
 import Collection from "../../collection";
-import Patch, { PatchConfig, PatchExtraErrorCodes } from "./patch";
+import Patch from "./patch";
 
-export default class PatchCollection extends Collection<
-  Patch,
-  PatchConfig,
-  PatchExtraErrorCodes
-> {
+export default class PatchCollection extends Collection<Patch> {
   protected id = "PatchCollection";
 
   protected init(directoryPath: string, resourceBag: ResourceBag): Patch {
